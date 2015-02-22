b712af1baca5ac47cb2b0ad2144d4c6d37f8a2c3
Steph Reaves <stephanie.reaves@gmail.com>
2015-02-22 16:03:19 -0500

    basic loop to capture the data and append each title

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
-	console.log("hi")
 	$.ajax({
-		url: 'https://api.flickr.com/services/feeds/photos_public.gne?&format=json',
+		url: 'https://api.flickr.com/services/feeds/photos_public.gne?&format=json&jsoncallback=wootwoot',
 		type: 'GET',
 		dataType: 'jsonp'
 	})
-	.done(function() {
-		console.log("success");
+	.success(function() {
+		wootwoot()
 	});
 
 });
+
+function wootwoot(data) {
+	var info = data.items
+	for(var i = 0; i < info.length; i++) {
+		$('#dashboard').append(info[i].title)
+	}
+}
