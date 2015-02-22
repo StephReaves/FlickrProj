05342c712789e0764cbe01cb1ef3d766d085b74c
Steph Reaves <stephanie.reaves@gmail.com>
2015-02-22 17:56:53 -0500

    polling of images complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,25 @@
 $(document).ready(function(){
-	$.ajax({
-		url: 'https://api.flickr.com/services/feeds/photos_public.gne?&format=json&jsoncallback=wootwoot',
-		type: 'GET',
-		dataType: 'jsonp'
-	})
-	.success(function() {
-		wootwoot()
-	});
+	apiCall();
 
 });
 
+var apiCall = function() {
+$.ajax({
+	url: 'https://api.flickr.com/services/feeds/photos_public.gne?&format=json&jsoncallback=wootwoot',
+	type: 'GET',
+	dataType: 'jsonp'
+})
+.success(function() {
+	wootwoot()
+	});
+setTimeout(apiCall, 10000)
+}
+
+setTimeout(apiCall, 10000)
+
 function wootwoot(data) {
 	var info = data.items
+	$('#dashboard').empty()
 	for(var i = 0; i < info.length; i++) {
 		$('#dashboard').append(info[i].title)
 		$('#dashboard').append(info[i].author)
